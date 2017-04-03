cc3e352cae37fe3e785215d5df7754677c9283d5
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-02 21:24:59 -0400

    StringTrimmerEditor as InitBinder

--- src/com/raj/spring/CustomerController.java
+++ src/com/raj/spring/CustomerController.java
@@ -2,9 +2,12 @@ package com.raj.spring;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,11 +24,18 @@ public class CustomerController {
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, 
 			BindingResult bindingResult){
+		System.out.println("Last Name : " +"|" + theCustomer.getLastName() + "|");
 		if(bindingResult.hasErrors()){
 			return "customer-form";
 		}
 		else{
 			return "customer-confirmation";
 		}
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder){
+		StringTrimmerEditor customEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, customEditor);
 	}
 }
